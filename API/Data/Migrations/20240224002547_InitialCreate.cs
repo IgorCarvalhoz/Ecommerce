ffbeea1c320944e021efc35c5eb974075823b9f7
Igor Carvalho <ig.carvalho001@gmail.com>
2024-02-23 21:57:31 -0300

    setting database

---
+++ API/Data/Migrations/20240224002547_InitialCreate.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            // Adiciona a criação da tabela __EFMigrationsHistory
+            migrationBuilder.CreateTable(
+                name: "__EFMigrationsHistory",
+                columns: table => new
+                {
+                    MigrationId = table.Column<string>(type: "TEXT", nullable: false),
+                    ProductVersion = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK___EFMigrationsHistory", x => x.MigrationId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            // Remove a tabela __EFMigrationsHistory no Down, se necessário
+            migrationBuilder.DropTable(
+                name: "__EFMigrationsHistory");
+        }
+    }
+}
+
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,4 @@
-using API.Controllers.Data;
+using API.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
